6f0aa0601425e91786d4031ac55baa520eb4dc43
Toshikazu808 <ryantkanno@gmail.com>
2021-05-11 15:26:47 -0700

    Initial commit

---
+++ InAndOut/Controllers/AppointmentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            //string todaysDate = DateTime.Now.ToShortDateString();
+            //return Ok(todaysDate);
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Ok("You have entered id = " + id);
+        }
+
+        // The URL pattern for routing is generated and added to the domain name
+        // ex. domain name is https://localhost:1337
+        // Then comes the controller
+        // Then the action
+        // And finally the id
+        // https://localhost:1337/{controller}/{action}/{id}/
+
+    }
+}
---
+++ InAndOut/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using InAndOut.Models;
+
+namespace InAndOut.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            // We need this constructor in order to use our data base, and store data in it
+        }
+        // These are the table that will be created in our database
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<ExpenseType> ExpenseTypes { get; set; }
+    }
+}
